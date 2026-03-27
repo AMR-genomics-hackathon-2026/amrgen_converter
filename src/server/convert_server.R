06661ea6cafb40ef26b4c7c54639efb795996072
Erkison <erkisonodih@gmail.com>
2026-03-27 15:59:16 +0000

    initial commit

--- src/server/convert_server.R
+++ src/server/convert_server.R
@@ -17,12 +17,7 @@ output$amr_data_table <- DT::renderDataTable({
 
 shiny::observeEvent(input$export_data,{
     shiny::req(amr_data(), input$export_data)
-    # apply filters
-    for (filter_col in input$filter_data_columns){
-        input_id <- paste0("filter_data_options_", filter_col)
-        filter_values <- input[[input_id]]
-        final_data$metadata <- filter_data(final_data$metadata, filter_col, filter_values)
-    }
+    # add logic to handle export
 })
 
 
